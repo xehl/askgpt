fd7da155c7c0487d29fb5ca953df6d6937ec6ec9
ehlee95 <eric95lee@gmail.com>
2023-02-16 23:19:15 -0500

    injecting element into dom on search pages

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,22 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
+console.log("This prints to the console of the google page");
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+// when a google search page is detected, send a message to the background script
+// background script will handle the api call to OpenAI server
+// and send the response back to the content script
+
+// TODO:
+// 1. send message to background script
+// 2. receive response from background script
+// 3. generate UI component to display response + add styles
+// 4. append UI component to DOM
+
+// extabar is the div on search results to append to
+let exta = document.querySelector("#extabar");
+let injected = document.createElement("div");
+injected.innerHTML = "Hello World";
+injected.style.backgroundColor = "red";
+
+let results = document.querySelector(".LHJvCe");
+injected.style.width = results.scrollWidth;
+
+exta.insertBefore(injected, null);
